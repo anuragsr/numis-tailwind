f5d6fef15122a86f5e26c07f303a55aa5def45f8
anuragsr <anurag.131092@gmail.com>
2022-02-10 10:54:52 +0530

    First commit

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -41,7 +41,7 @@ module.exports = {
     // Generates an HTML file from a template
     // Generates deprecation warning: https://github.com/jantimon/html-webpack-plugin/issues/1501
     new HtmlWebpackPlugin({
-      title: 'Webpack + Tailwind Starter',
+      title: 'Flashy Frontend',
       favicon: paths.src + '/images/favicon.png',
       template: paths.src + '/index.html', // template file
       filename: 'index.html', // output file
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-// Test import of styles
-import '@/styles/index.scss'
+// // Test import of styles
+// import '@/styles/index.scss'
 
 // Test import of tailwind
 import '@/styles/tw.css'
