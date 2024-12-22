349ecf45678ce82e3261b1c52e5072fc4be25290
Anurag <anurag.131092@gmail.com>
2024-12-22 15:27:34 +0530

    Added bg video, removed sass

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -41,7 +41,7 @@ module.exports = {
     // Generates an HTML file from a template
     // Generates deprecation warning: https://github.com/jantimon/html-webpack-plugin/issues/1501
     new HtmlWebpackPlugin({
-      title: 'Flashy Frontend',
+      title: 'Numis',
       favicon: paths.src + '/images/favicon.png',
       template: paths.src + '/index.html', // template file
       filename: 'index.html', // output file
