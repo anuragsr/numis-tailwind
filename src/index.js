ceaf94ad6ebe315ba751de607667bd2a55bf2cf2
Anurag <anurag.131092@gmail.com>
2024-12-22 21:05:06 +0530

    Added bg video overlay & animation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-// // Test import of styles
-// import '@/styles/index.scss'
-import 'flowbite'
+// Test import of styles
+import "flowbite";
 
 // Test import of tailwind
-import '@/styles/tw.css'
+import "@/styles/index.css";
 
-// initFlowbite()
+import { gsap } from "gsap";
 
+const l = console.log.bind(window.console);
+
+l("init", gsap);
+
+gsap.to(".ctn-overlay > div", {
+  opacity: 0.1,
+  duration: 3,
+  stagger: {
+    // wrap advanced options in an object
+    each: 0.1,
+    // from: 'center',
+    from: "random",
+    grid: "auto",
+    ease: "power2.inOut",
+    yoyo: true,
+    repeat: -1, // Repeats immediately, not waiting for the other staggered animations to finish
+  },
+});
